65ba709c2c0bc88643a9e58faed429f493c7bcc5
juanjts <juanjo.4555@gmail.com>
2026-02-20 18:37:40 -0500

    install bulma css and add login page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import 'bulma/css/bulma.min.css'
 
 const app = mount(App, {
   target: document.getElementById('app'),
--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,7 +1,47 @@
 <script>
+// @ts-nocheck
 
+let userAccount = null;
+let pass = null;
+
+const handleLogin = async () => {
+    console.log('login')
+}
 </script>
 
-<div>
-    <p>login</p>
+
+<div class="column is-half is-flex is-flex-direction-column is-justify-content-center">
+    <h1 class="title has-text-centered">Login</h1>
+
+    <div class="field">
+        <label class="label has-text-left" for="userAccount">Email</label>
+        <div class="control">
+            <input class="input"
+            id="userAccount"
+            type="email"
+            placeholder="emmail"
+            bind:value={userAccount}>
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label has-text-left" for="pass">Contraseña</label>
+        <div class="control">
+            <input class="input"
+            id="pass"
+            type="password"
+            placeholder="Contraseña"
+            bind:value={pass}>
+        </div>
+    </div>
+
+    <div class="field">
+        <button class="button is-primary is-fullwidth" on:click={handleLogin()}>
+            Ingresar
+        </button>
+    </div>
 </div>
+
+<style>
+
+</style>
